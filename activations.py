4066de2044000f84de3d318f19de6bbdb3d1e28e
Bohdan <81767251+AdobyY@users.noreply.github.com>
2024-11-04 15:02:40 +0000

    Aded Genetic algo

--- activations.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class ReLU:
-    def forward(self, x):
-        return np.maximum(0, x)
-
-    def backward(self, x):
-        return (x > 0).astype(float)
-
--- optimizers.py
+++
@@ -1,8 +0,0 @@
-class GDOptimizer:
-    def __init__(self, eta=0.01):
-        self.eta = eta
-
-    def update(self, layer):
-        layer.weights -= self.eta * layer.dW
-        layer.biases -= self.eta * layer.db
-
